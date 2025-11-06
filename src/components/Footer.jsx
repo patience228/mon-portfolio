77387b9821a25de419e3c300107844635cf0a647
patience <patience@id2real.com>
2025-11-06 11:02:54 +0100

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Akou SIABI. Tous droits réservés.</p>
+    </footer>
+  );
+};
+
+export default Footer;
